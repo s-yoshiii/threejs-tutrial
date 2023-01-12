3da48dd21cdbb2e36e622bc48e06425bab6d9b9c
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-12 18:24:02 +0900

    環境構築

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from "vite";
+import path from "path";
+const root = "src";
+export default defineConfig({
+  root: root,
+  base: "/",
+  publicDir: "public",
+  server: {
+    port: 8080,
+    open: true,
+  },
+  css: {
+    devSourcemap: true,
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        style: path.resolve(__dirname, "./src/style.scss"),
+        main: path.resolve(__dirname, "./src/index.html"),
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split(".").at(1);
+          console.log(assetInfo);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = "img";
+            return `assets/${extType}/[name][extname]`;
+          } else if (/s?css$/i.test(extType)) {
+            extType = "css";
+            return `assets/${extType}/[name][extname]`;
+          }
+          return `assets/[name][extname]`;
+        },
+        chunkFileNames: "assets/js/main.js",
+        entryFileNames: "assets/js/[name].js",
+      },
+    },
+  },
+});
