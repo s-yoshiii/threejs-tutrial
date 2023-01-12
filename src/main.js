223c040da6578a96ea8f7c31633e7affa4c80749
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-12 23:46:51 +0900

    処理の流れを確認しよう

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log("aaa");
+import * as THREE from "three";
+
+// scene ステージ
+let scene,
+  box,
+  camera,
+  width = 500,
+  height = 250;
+
+// mesh 物体
+// -geometry 形状
+// -material 材質
+
+// camera
