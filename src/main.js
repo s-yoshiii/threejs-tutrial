6e84c1259820e6fa4c461e5da6a04095922bf721
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-14 17:03:00 +0900

    Materialについて見ていこう

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ let scene,
   head,
   body,
   light,
-  sphere,
+  sphere1,
+  sphere2,
+  sphere3,
   plane,
   ambient,
   camera,
@@ -46,13 +48,29 @@ person.add(head);
 person.add(body);
 scene.add(person);
 
-// sphere
-sphere = new THREE.Mesh(
+// sphere1
+sphere1 = new THREE.Mesh(
   new THREE.SphereGeometry(50, 20, 20),
-  new THREE.MeshLambertMaterial({ color: 0xff0000 })
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
 );
-sphere.position.set(100, 0, 0);
-scene.add(sphere);
+sphere1.position.set(100, 0, -150);
+scene.add(sphere1);
+
+// sphere2
+sphere2 = new THREE.Mesh(
+  new THREE.SphereGeometry(50, 50, 50),
+  new THREE.MeshLambertMaterial({ color: 0xff0000, wireframe: true })
+);
+sphere2.position.set(100, 0, 0);
+scene.add(sphere2);
+
+// sphere3
+sphere3 = new THREE.Mesh(
+  new THREE.SphereGeometry(50, 20, 20),
+  new THREE.MeshPhongMaterial({ color: 0xff0000 })
+);
+sphere3.position.set(100, 0, 150);
+scene.add(sphere3);
 
 // plane
 plane = new THREE.Mesh(
@@ -76,7 +94,7 @@ camera.position.set(200, 100, 300);
 camera.lookAt(scene.position);
 
 // helper
-gridHelper = new THREE.GridHelper(200, 20);
+gridHelper = new THREE.GridHelper(500, 20);
 scene.add(gridHelper);
 axisHelper = new THREE.AxesHelper(1000);
 scene.add(axisHelper);
